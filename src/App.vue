9b563495bb6c5363e6610ef4475faa24e3298ddf
Chris Yap <chris.yap@medicaldirector.com>
2017-12-04 08:28:49 +1100

    comment off html

--- src/App.vue
+++ src/App.vue
@@ -231,26 +231,26 @@
       //   !this.$v.checkbox.required && errors.push('You must agree to continue!')
       //   return errors
       // },
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.name.required && errors.push('Name is required.')
-        return errors
-      },
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      }
+      // selectErrors () {
+      //   const errors = []
+      //   if (!this.$v.select.$dirty) return errors
+      //   !this.$v.select.required && errors.push('Item is required')
+      //   return errors
+      // },
+      // nameErrors () {
+      //   const errors = []
+      //   if (!this.$v.name.$dirty) return errors
+      //   !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+      //   !this.$v.name.required && errors.push('Name is required.')
+      //   return errors
+      // },
+      // emailErrors () {
+      //   const errors = []
+      //   if (!this.$v.email.$dirty) return errors
+      //   !this.$v.email.email && errors.push('Must be valid e-mail')
+      //   !this.$v.email.required && errors.push('E-mail is required')
+      //   return errors
+      // }
     }
   }
 </script>
